05d80cab0f0b959944d9a96df6561f4992544125
gimhnaay <gimhanifernando04@gmail.com>
2024-02-05 23:55:02 +0530

    first comit

---
+++ src/main/java/org/example/service/Impl/BookServiceImpl.java
@@ -0,0 +1,13 @@
+package org.example.service.Impl;
+
+import org.example.dto.Book;
+import org.example.service.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Override
+    public void addBook(Book book) {
+
+    }
+}
